a9a978e746b679ba81cc2513f308d7ac6a8f34ae
90capsules <90capsules@users.noreply.github.com>
2015-07-31 20:22:14 +0900

    Create ThirdGreatest.rb

---
+++ ThirdGreatest.rb
@@ -0,0 +1,14 @@
+def ThirdGreatest(strArr)
+  
+  
+  strArr.delete(strArr.max_by{|x| x.length})
+  strArr.delete(strArr.max_by{|x| x.length})
+  
+  # code goes here
+  return strArr.max_by{|x| x.length}
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ThirdGreatest(STDIN.gets)
